061d84210a3fea808feca2cf3b8dce449f1e99e9
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-03-25 16:11:09 -0700

    navbar start

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import pokemonapilogo from "@/public/assets/pokemonapilogoremoved.png";
+import { Navbar, TextInput } from "flowbite-react";
 
 export default function Home() {
   return (
     <div>
-      
+      <Navbar fluid className="bg-[#3854A1]">
+        <Image
+          loading="eager"
+          alt="notesicon"
+          src={pokemonapilogo}
+          style={{ maxWidth: "20%", height: "auto%" }}
+        />
+        <div>
+          <TextInput
+            placeholder="search contacts..."
+            className="rounded-lg bg-white text-black"
+          />
+        </div>
+      </Navbar>
     </div>
   );
 }
